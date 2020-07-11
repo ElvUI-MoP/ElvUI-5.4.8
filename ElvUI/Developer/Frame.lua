5191293da12aac68093150b5792dec4546fefafc
Apollyonn <Silentchaos@hotmail.gr>
2020-07-12 00:24:16 +0300

    update /frame cmd
    allows you to retrieve info for all objects that are related to the
    mouseover frame
    eg: /frame .parent
    /frame :GetParent()

--- ElvUI/Developer/Frame.lua
+++ ElvUI/Developer/Frame.lua
@@ -1,6 +1,6 @@
 local _G = _G
 local loadstring, pcall, print, tostring, type, select = loadstring, pcall, print, tostring, type, select
-local find, format = string.find, string.format
+local find, format, match = string.find, string.format, string.match
 local tconcat = table.concat
 
 local UIParentLoadAddOn = UIParentLoadAddOn
@@ -47,13 +47,33 @@ local function getObject(objName)
 	else
 		obj = _G[objName]
 
-		if not obj and find(objName, "[%.()%[%]'\"]") then
-			local success, ret = pcall(loadstring("return " .. objName))
-			if success then
-				ret = ret == "string" and _G[ret] or ret
+		if not obj then
+			local pass
 
-				if type(ret) == "table" and ret.GetName then
-					obj = ret
+			if find(objName, "^[%.:]([A-z0-9_]+)") then
+				local _obj = GetMouseFocus()
+
+				if _obj and _obj ~= WorldFrame then
+					local res = match(objName, "^[%.:]([A-z0-9_]+)")
+
+					if res and _obj[res] and _obj:GetName() then
+						objName = format("%s%s", _obj:GetName(), objName)
+					end
+
+					pass = true
+				end
+			elseif find(objName, "[%.()%[%]'\"]") then
+				pass = true
+			end
+
+			if pass then
+				local success, ret = pcall(loadstring(format("return %s", objName)))
+				if success then
+					ret = ret == "string" and _G[ret] or ret
+
+					if type(ret) == "table" and ret.GetName then
+						obj = ret
+					end
 				end
 			end
 		end
@@ -143,7 +163,7 @@ end
 SLASH_TEXLIST1 = "/texlist"
 SlashCmdList.TEXLIST = function(frame)
 	frame = getObject(frame)
-	if not frame then return end
+	if not (frame and frame.GetNumRegions) then return end
 
 	printNoTimestamp("|cffCC0000----------------------------|r")
 	printNoTimestamp(format("Texture List of |cffFFD100%s|r", frame:GetName() or "nil"))
@@ -165,7 +185,7 @@ end
 SLASH_REGLIST1 = "/reglist"
 SlashCmdList.REGLIST = function(frame)
 	frame = getObject(frame)
-	if not frame then return end
+	if not (frame and frame.GetNumRegions) then return end
 
 	printNoTimestamp("|cffCC0000----------------------------|r")
 	printNoTimestamp(format("Region List of |cffFFD100%s|r", frame:GetName() or "nil"))
@@ -184,7 +204,7 @@ end
 SLASH_CHILDLIST1 = "/childlist"
 SlashCmdList.CHILDLIST = function(frame)
 	frame = getObject(frame)
-	if not frame then return end
+	if not (frame and frame.GetNumChildren) then return end
 
 	printNoTimestamp("|cffCC0000----------------------------|r")
 	printNoTimestamp(format("Child List of |cffFFD100%s|r", frame:GetName() or "nil"))
